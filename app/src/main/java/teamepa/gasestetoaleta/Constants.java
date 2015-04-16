feceab49e433002ac4e14ba18ebf542715f533f9
Silviu Pal <silviu.pal@live.com>
2015-04-16 10:52:16 +0300

    Adauga pagina de constante.

---
+++ app/src/main/java/teamepa/gasestetoaleta/Constants.java
@@ -0,0 +1,51 @@
+package teamepa.gasestetoaleta;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by SilviuPal on 16-Apr-15.
+ */
+public class Constants
+{
+	public static LatLng BUCHAREST    = new LatLng(44.4356442d, 26.1024104d);
+	public static LatLng BUCHAREST_1  = new LatLng(44.4376442d, 26.1027104d);
+	public static LatLng BUCHAREST_2  = new LatLng(44.4386442d, 26.1028104d);
+	public static LatLng BUCHAREST_3  = new LatLng(44.4396442d, 26.1029104d);
+	public static LatLng BUCHAREST_4  = new LatLng(44.4306442d, 26.1010104d);
+	public static LatLng BUCHAREST_5  = new LatLng(44.4316442d, 26.1051104d);
+	public static LatLng BUCHAREST_6  = new LatLng(44.4326442d, 26.1031104d);
+	public static LatLng BUCHAREST_7  = new LatLng(44.4356442d, 26.1081104d);
+	public static LatLng BUCHAREST_8  = new LatLng(44.4316322d, 26.1091104d);
+	public static LatLng BUCHAREST_9  = new LatLng(44.4316942d, 26.1341104d);
+	public static LatLng BUCHAREST_10 = new LatLng(44.4316124d, 26.1671104d);
+
+	public interface Title
+	{
+		public static final String BUCHAREST    = "Bucharest";
+		public static final String BUCHAREST_1  = "Bucharest";
+		public static final String BUCHAREST_2  = "Bucharest";
+		public static final String BUCHAREST_3  = "Bucharest";
+		public static final String BUCHAREST_4  = "Bucharest";
+		public static final String BUCHAREST_5  = "Bucharest";
+		public static final String BUCHAREST_6  = "Bucharest";
+		public static final String BUCHAREST_7  = "Bucharest";
+		public static final String BUCHAREST_8  = "Bucharest";
+		public static final String BUCHAREST_9  = "Bucharest";
+		public static final String BUCHAREST_10 = "Bucharest";
+	}
+
+	public interface Snippet
+	{
+		public static final String BUCHAREST    = "Bucharest is amazing!";
+		public static final String BUCHAREST_1  = "Super Toaleta!";
+		public static final String BUCHAREST_2  = "Curat si ingrijit!";
+		public static final String BUCHAREST_3  = "La parterul unui bloc, o doamna draguta ofera acces la toaleta ei.";
+		public static final String BUCHAREST_4  = "Merita drumul! Este intr-un restaurant.";
+		public static final String BUCHAREST_5  = "Nu e cine stie ce, dar suficient pentru orice nevoi!";
+		public static final String BUCHAREST_6  = "Este in KFC.";
+		public static final String BUCHAREST_7  = "Intercontinental.";
+		public static final String BUCHAREST_8  = "McDonalds";
+		public static final String BUCHAREST_9  = "In Pub 13.";
+		public static final String BUCHAREST_10 = "Excelenta! Restaurant Perla.";
+	}
+}
